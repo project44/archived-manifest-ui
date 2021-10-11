184e0891149d7acdc2c8abb339a0332ba664bad0
Sam Rose <srose@project44.com>
2021-10-11 10:00:54 -0400

    chore(examples): Update single-spa example

--- examples/single-spa/app-parcel/src/root.component.tsx
+++ examples/single-spa/app-parcel/src/root.component.tsx
@@ -1,12 +1,12 @@
-import { Box, Button, ChakraProvider } from '@chakra-ui/react';
+import { Box, Button, ManifestProvider } from '@manifest-ui/react';
 
 export default function Root(props) {
   return (
-    <ChakraProvider>
+    <ManifestProvider>
       <Box as="section" m="14">
         {props.name} is
         <Button>Mounted</Button>
       </Box>
-    </ChakraProvider>
+    </ManifestProvider>
   );
 }
