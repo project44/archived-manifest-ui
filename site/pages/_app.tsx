2f8975d6c0ffcfcfb97788126ab9b94912b5d01a
Sam Rose <srose@project44.com>
2021-10-25 09:26:20 -0500

    fix(site): restore global base element styles

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,12 +1,67 @@
 import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import { ManifestProvider } from '@manifest-ui/react';
+import { ChakraTheme, extendTheme, ManifestProvider } from '@manifest-ui/react';
+import baseTheme from '@manifest-ui/themes';
 import { Global } from '@emotion/react';
+
+type ThemeFn = {
+  colorMode: 'light' | 'dark';
+  theme: ChakraTheme;
+};
+
+const theme = extendTheme(
+  {
+    styles: {
+      global: ({ colorMode }: ThemeFn) => ({
+        h1: {
+          mt: '8',
+          mb: '1',
+          fontSize: '3xl',
+          fontWeight: 'bold',
+          lineHeight: '1.2',
+          letterSpacing: 'tight',
+        },
+        h2: {
+          mt: '24',
+          mb: '2',
+          fontSize: '2xl',
+          fontWeight: 'semibold',
+          lineHeight: '1.3',
+          letterSpacing: 'tight',
+        },
+        h3: {
+          mt: '12',
+          mb: '2',
+          fontSize: 'xl',
+          fontWeight: 'semibold',
+          lineHeight: 'shorter',
+          letterSpacing: 'tight',
+        },
+        p: {
+          mt: '5',
+          lineHeight: '1.7',
+        },
+        code: {
+          borderRadius: 'sm',
+          paddingInlineStart: '1',
+          paddingInlineEnd: '1',
+          fontSize: 'sm',
+          paddingTop: '2px',
+          paddingBottom: '2px',
+          whiteSpace: 'nowrap',
+          lineHeight: 'normal',
+          color: colorMode === 'dark' ? 'purple.200' : 'purple.500',
+        },
+      }),
+    },
+  },
+  baseTheme,
+);
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ManifestProvider>
+    <ManifestProvider theme={theme}>
       <Global
         styles={{
           'html, body': {
