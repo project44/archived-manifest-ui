ec4c191aae93044aced7bf3db9a6376a1ae1d899
Sam Rose <srose@project44.com>
2021-10-25 09:10:44 -0500

    chore(eslint): include eslint-plugin-import in linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
