547b164be69934f711cbc82fdbb04cd559409ea4
Sam Rose <srose@project44.com>
2021-10-29 13:51:48 -0500

    fix(Playroom): update config to work with new Stitches components

--- .playroom/FrameComponent.tsx
+++ .playroom/FrameComponent.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { ChakraTheme, ManifestProvider } from '../packages/react/src';
+import { globalCss, theme } from '../packages/react/src';
 
 interface Props {
-  theme: ChakraTheme;
+  theme: typeof theme;
   children: React.ReactNode;
 }
 
-export default ({ theme, children }: Props) => (
-  <>
-    <ManifestProvider theme={theme}>{children}</ManifestProvider>
-  </>
-);
+const globalStyles = globalCss({
+  '@import':
+    "url('https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&display=swap')",
+  'html, body': {
+    margin: 0,
+  },
+});
+
+export default ({ theme, children }: Props) => {
+  globalStyles();
+
+  return <div className={theme}>{children}</div>;
+};
