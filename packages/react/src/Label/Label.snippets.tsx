b099bc80d0bb92b5045b6833147b3d4bb48aedbb
Sam Rose <srose@project44.com>
2021-11-02 20:43:08 -0400

    refactor(react): simplify and decouple Label

--- packages/react/src/Label/Label.snippets.tsx
+++ packages/react/src/Label/Label.snippets.tsx
@@ -3,7 +3,12 @@ import { Snippets } from 'playroom';
 export const snippets: Snippets = [
   {
     group: 'Label',
-    name: 'Normal',
+    name: 'Standard',
     code: `<Label>Label</Label>`,
+  },
+  {
+    group: 'Label',
+    name: 'Small',
+    code: `<Label size="small">Label</Label>`,
   },
 ];
