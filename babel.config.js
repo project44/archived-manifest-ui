1265f7ec1d71843266e39f2d73097118bf674b8f
Sam Rose <srose@project44.com>
2021-11-11 16:32:38 -0500

    feat(testing-utils): add testing utils

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,20 @@ module.exports = {
   presets: [
     '@babel/preset-typescript',
     '@babel/preset-react',
-    ['@babel/preset-env', { targets: 'defaults' }],
+    [
+      '@babel/preset-env',
+      {
+        bugfixes: true,
+        exclude: [
+          '@babel/plugin-transform-regenerator',
+          '@babel/plugin-transform-async-to-generator',
+        ],
+        loose: false,
+        modules: 'auto',
+        shippedProposals: true,
+        targets: { node: 'current' },
+        useBuiltIns: false,
+      },
+    ],
   ],
 };
