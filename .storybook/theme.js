4f0895b5c8468ef359ce95f2ec5cf2cd0b2d27a8
Erin Mahoney <88858003+eemahoney@users.noreply.github.com>
2022-01-28 08:37:47 -0600

    chore: add Manifest UI logo to SB (#113)

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Manifest UI',
+  brandUrl: 'https://p44design.systems',
+  brandImage: 'manifest.png',
+});
