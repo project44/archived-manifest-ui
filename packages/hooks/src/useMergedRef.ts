3af9f4bc36ce78f9b5711254512836ccbac4fd8c
Brandon Clark <bclark@project44.com>
2022-03-28 10:12:32 -0500

    chore: fixing lint

--- packages/hooks/src/useMergedRef.ts
+++ packages/hooks/src/useMergedRef.ts
@@ -2,6 +2,5 @@ import * as React from 'react';
 import { mergeRefs } from '@manifest-ui/utils';
 
 export function useMergedRef<T>(...refs: (React.Ref<T> | undefined)[]) {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   return React.useCallback(mergeRefs(...refs), refs);
 }
