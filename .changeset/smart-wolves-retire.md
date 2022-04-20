60bbd752278642f269069c04d170dfc9f6de5c0c
Brandon Clark <yhbrandon@gmail.com>
2022-04-20 10:20:19 -0500

    Feat/data viz tokens (#212)
    * feat(tokens): adding data-viz tokens
    
    * feat(tokens): updating code owners
    
    * chore: adding changeset

---
+++ .changeset/smart-wolves-retire.md
@@ -0,0 +1,5 @@
+---
+'@manifest-ui/tokens': minor
+---
+
+Adding data visually design tokens
--- packages/tokens/src/getToken.ts
+++ packages/tokens/src/getToken.ts
@@ -1,7 +1,7 @@
 import TOKEN_MAP from './generated/tokenMap';
 
-export function getToken<T extends keyof typeof TOKEN_MAP>(path: T) {
-  const token = TOKEN_MAP[path];
+export function getToken<T extends keyof typeof TOKEN_MAP>(path: T): string | number {
+  const token: string | number = TOKEN_MAP[path];
 
   if (process.env.NODE_ENV !== 'production' && !token) {
     console.warn(`[Manifest UI] unknown design token at path: ${path}`);
